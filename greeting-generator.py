f630114311b09a1b485f174d9bdef3b64f74807c
Elias Schmalzried <elias.schmalzried@gmx.net>
2024-11-04 16:19:28 +0100

    Erste Version

---
+++ greeting-generator.py
@@ -0,0 +1,14 @@
+print("---Willkommen zum Geburtstagskarten-Generator---")
+
+name = input("Name der zu grüßenden Person eingeben: ")
+age = input("Alter der zu grüßenden Person eingeben: ")
+sender = input("Eingenen Namen eingeben: ")
+
+print("Hallo " + name + ",")
+print("Ich wünsche dir alles Gute zum " + age + "ten Geburtstag. ")
+print("Ich hoffe das du einen schönen Tag hast und mit deinen liebsten feierst. ")
+print("Zudem wünsche ich dir alles nur erdenklich Gute für das neue Lebensjahr und viel Gesundheit. ")
+print("Liebe Grüße")
+print(sender)
+
+
